63a281db52bce60700367088860b4eb14a909ba8
seqi <tomseqi@live.co.uk>
2019-07-17 18:12:35 +0100

    Install zsh-autosuggestions

--- install.sh
+++ install.sh
@@ -4,5 +4,8 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 # Install syntax highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
+# Install autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
 # Move dot files
 bassh ./update.sh
