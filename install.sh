f1ef98976930579bd3db09c30dbf38fda8af3e20
seqi <tomseqi@live.co.uk>
2019-07-17 18:10:33 +0100

    Install zsh-syntax-highlighting

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+# Install syntax highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
 # Move dot files
-sh ./update.sh
+bassh ./update.sh
